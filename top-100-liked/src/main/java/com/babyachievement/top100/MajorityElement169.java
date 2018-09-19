7380066fe79de3b5b26da77143e25f17087085ff
haoqiang <haoqiang@oneapm.com>
2018-09-19 21:30:01 +0800

    add solution for Major Element

---
+++ top-100-liked/src/main/java/com/babyachievement/top100/MajorityElement169.java
@@ -0,0 +1,28 @@
+/**
+ * date: 2018/9/19 18:27
+ */
+
+package com.babyachievement.top100;
+
+/**
+ * https://leetcode.com/problems/majority-element/description/
+ */
+public class MajorityElement169 {
+    public int majorityElement(int[] nums) {
+        int major = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                major = nums[i];
+                count++;
+            } else if (major == nums[i]) {
+                count++;
+            }else {
+                count--;
+            }
+        }
+
+        return major;
+
+    }
+}
