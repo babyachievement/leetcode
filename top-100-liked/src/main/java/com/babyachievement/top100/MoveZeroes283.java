4b372ce0352f2bcb620fc2ff1d4f63c8dea631de
haoqiang <haoqiang@oneapm.com>
2018-09-25 19:33:03 +0800

    add solution for Move Zero

--- top-100-liked/src/main/java/com/babyachievement/top100/MoveZeroes283.java
+++ top-100-liked/src/main/java/com/babyachievement/top100/MoveZeroes283.java
@@ -9,6 +9,18 @@ package com.babyachievement.top100;
  */
 public class MoveZeroes283 {
     public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int countOfNonZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[countOfNonZero++] = nums[i];
+            }
+        }
 
+        for (int i = countOfNonZero; i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
