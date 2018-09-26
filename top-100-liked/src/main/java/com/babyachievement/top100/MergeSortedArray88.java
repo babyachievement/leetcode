a692024333b4e418fc8484b99688231a9caaa9d9
haoqiang <haoqiang@oneapm.com>
2018-09-26 14:50:20 +0800

    add solution for Merge Sorted Array

---
+++ top-100-liked/src/main/java/com/babyachievement/top100/MergeSortedArray88.java
@@ -0,0 +1,30 @@
+/**
+ * date: 2018/9/26 14:43
+ */
+
+package com.babyachievement.top100;
+
+/**
+ * https://leetcode.com/problems/merge-sorted-array/description/
+ */
+public class MergeSortedArray88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[i + j + 1] = nums1[i];
+                i--;
+            } else {
+                nums1[i + j + 1] = nums2[j];
+                j--;
+            }
+        }
+
+        while (j >= 0) {
+            nums1[i + j + 1] = nums2[j];
+            j--;
+        }
+    }
+}
