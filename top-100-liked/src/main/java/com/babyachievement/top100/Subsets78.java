74f34425518c9ac62474d58f24994b36e6f06f73
haoqiang <haoqiang@oneapm.com>
2018-09-28 17:22:25 +0800

    add TODO tasks

---
+++ top-100-liked/src/main/java/com/babyachievement/top100/Subsets78.java
@@ -0,0 +1,22 @@
+/**
+ * date: 2018/9/28 17:14
+ */
+
+package com.babyachievement.top100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/subsets/description/
+ */
+public class Subsets78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        final List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        // TODO
+
+        return result;
+    }
+}
