a588646b6d91f705f67838ed4843f8780c96394e
haoqiang <haoqiang@oneapm.com>
2018-10-08 18:36:31 +0800

    add solution for Queue Reconstuction by Height

--- top-100-liked/src/main/java/com/babyachievement/top100/QueueReconstructionByHeight406.java
+++ top-100-liked/src/main/java/com/babyachievement/top100/QueueReconstructionByHeight406.java
@@ -4,8 +4,10 @@
 
 package com.babyachievement.top100;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * https://leetcode.com/problems/queue-reconstruction-by-height/description/
@@ -23,7 +25,46 @@ public class QueueReconstructionByHeight406 {
             }
         });
 
-        // TODO
-        return null;
+
+        boolean[] used = new boolean[people.length];
+
+        int[][] result = new int[people.length][2];
+
+        for (int[] person : people) {
+            int preNum = person[1];
+
+
+            int i = 0;
+            for (; preNum != 0 || used[i]; i++) {
+                if (!used[i]) {
+                    preNum--;
+                } else if (result[i][0] >= person[0]) {
+                    preNum--;
+                }
+            }
+
+            result[i] = person;
+            used[i] = true;
+        }
+
+        return result;
+    }
+
+    public int[][] reconstructQueue2(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] p1, int[] p2) {
+                if (p1[0] == p2[0]) {
+                    return p1[1] - p2[1];
+                } else {
+                    return p2[0] - p1[0];
+                }
+            }
+        });
+
+        List<int[]> res = new ArrayList<>();
+        for (int[] p : people) {
+            res.add(p[1], p);
+        }
+        return res.toArray(new int[0][0]);
     }
 }
--- top-100-liked/src/test/java/com/babyachievement/top100/QueueReconstructionByHeight406Test.java
+++ top-100-liked/src/test/java/com/babyachievement/top100/QueueReconstructionByHeight406Test.java
@@ -12,7 +12,8 @@ public class QueueReconstructionByHeight406Test {
     @Test
     public void reconstructQueue() {
         final QueueReconstructionByHeight406 reconstruction = new QueueReconstructionByHeight406();
-        final int[][] reconstructQueue = reconstruction.reconstructQueue(new int[][]{{7, 0}, {4, 4}, {7, 1}, {5,
+        final int[][] reconstructQueue = reconstruction.reconstructQueue2(new
+                                                                                  int[][]{{7, 0}, {4, 4}, {7, 1}, {5,
                 0}, {6, 1}, {5, 2}});
         final int[][] expected = {
                 {5, 0}, {7, 0}, {5, 2}, {6, 1}, {4, 4}, {7, 1}
