3937d23b9597d5b29f222e1237dc27085daed623
haoqiang <haoqiang@oneapm.com>
2018-10-18 14:59:53 +0800

    add  top interview questions module

---
+++ top-interview-questions/src/main/java/com/babyachievement/top/interviews/FractionToRecurringDecimal166.java
@@ -0,0 +1,15 @@
+/**
+ * date: 2018/10/18 14:55
+ */
+
+package com.babyachievement.top.interviews;
+
+/**
+ * https://leetcode.com/problems/fraction-to-recurring-decimal/description/
+ */
+public class FractionToRecurringDecimal166 {
+    public String fractionToDecimal(int numerator, int denominator) {
+        // TODO
+        return null;
+    }
+}
---
+++ top-interview-questions/src/test/java/com/babyachievement/top/interviews/FractionToRecurringDecimal166Test.java
@@ -0,0 +1,15 @@
+package com.babyachievement.top.interviews;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * date: 2018/10/18 14:59
+ */
+public class FractionToRecurringDecimal166Test {
+
+    @Test
+    public void fractionToDecimal() {
+    }
+}
