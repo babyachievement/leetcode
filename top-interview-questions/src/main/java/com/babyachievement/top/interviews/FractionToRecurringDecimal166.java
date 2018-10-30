cf2f7a4ee3c70420b1d96b5f6a70e8e404707ddb
haoqiang <haoqiang@oneapm.com>
2018-10-30 18:02:12 +0800

    add solution for Fraction To Recuring Decimal

--- top-interview-questions/src/main/java/com/babyachievement/top/interviews/FractionToRecurringDecimal166.java
+++ top-interview-questions/src/main/java/com/babyachievement/top/interviews/FractionToRecurringDecimal166.java
@@ -4,12 +4,106 @@
 
 package com.babyachievement.top.interviews;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.com/problems/fraction-to-recurring-decimal/description/
  */
 public class FractionToRecurringDecimal166 {
     public String fractionToDecimal(int numerator, int denominator) {
-        // TODO
-        return null;
+        long num1 = numerator;
+        long num2 = denominator;
+        final long i1 = num1 / num2;
+
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)) {
+            if (Math.abs(num1) < Math.abs(num2)) {
+                stringBuilder.append("-");
+            }
+        }
+
+        stringBuilder.append(i1);
+
+        Map<Long, Long> map = new HashMap<>();
+        if (num1 % num2 != 0) {
+            stringBuilder.append(".");
+            calPoint(Math.abs(num1 % num2), Math.abs(num2),
+                     stringBuilder,
+                     map);
+        }
+
+
+        return stringBuilder.toString();
+    }
+
+    private void calPoint(long numerator, long denominator, StringBuilder
+            stringBuilder, Map<Long, Long> map) {
+        System.out.println(numerator);
+        if (map.containsKey(numerator)) {
+            stringBuilder.insert(map.get(numerator).intValue(), "(");
+            stringBuilder.append(")");
+            return;
+        }
+
+
+        if (numerator % denominator == 0) {
+            stringBuilder.append(numerator / denominator);
+            return;
+        } else {
+            if (numerator > denominator) {
+                stringBuilder.append(numerator / denominator);
+                map.put(numerator, stringBuilder.length() - 1L);
+                calPoint(numerator % denominator,
+                         denominator,
+                         stringBuilder, map);
+            } else {
+                if (numerator < denominator) {
+                    map.put(numerator, stringBuilder.length() +0L);
+                }
+                numerator = numerator * 10;
+                while (numerator < denominator) {
+                    map.put(numerator, stringBuilder.length()+1L);
+                    numerator *= 10;
+                    stringBuilder.append("0");
+                }
+
+                calPoint(numerator, denominator, stringBuilder, map);
+            }
+        }
+    }
+
+    public String fractionToDecimal2(int numerator, int denominator) {
+        if (denominator==0||numerator==0)return "0";
+        StringBuilder sb=new StringBuilder();
+        if((numerator<0)^(denominator<0))
+            sb.append("-");
+        long a=Math.abs((long)numerator);
+        long b=Math.abs((long)denominator);
+        sb.append(a/b);
+        a=a%b;
+        if(a==0)
+            return sb.toString();
+        sb.append(".");
+        HashMap<Long,Integer>hm=new HashMap<>();
+        hm.put(a,sb.length());
+        while(a!=0){
+            a=a*10;
+            sb.append(a/b);
+
+            a=a%b;
+            if(hm.containsKey(a)){
+                sb.insert(hm.get(a),"(");
+                sb.append(")");
+                return sb.toString();
+            }
+            hm.put(a,sb.length());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(-7 / 12);
     }
 }
--- top-interview-questions/src/test/java/com/babyachievement/top/interviews/FractionToRecurringDecimal166Test.java
+++ top-interview-questions/src/test/java/com/babyachievement/top/interviews/FractionToRecurringDecimal166Test.java
@@ -2,7 +2,8 @@ package com.babyachievement.top.interviews;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * date: 2018/10/18 14:59
@@ -11,5 +12,19 @@ public class FractionToRecurringDecimal166Test {
 
     @Test
     public void fractionToDecimal() {
+        final FractionToRecurringDecimal166 recurringDecimal166 = new FractionToRecurringDecimal166();
+        assertThat(recurringDecimal166.fractionToDecimal(1, 2), is("0.5"));
+        assertThat(recurringDecimal166.fractionToDecimal(1, 2), is("0.5"));
+        assertThat(recurringDecimal166.fractionToDecimal(2, 1), is("2"));
+        assertThat(recurringDecimal166.fractionToDecimal(2, 3), is("0.(6)"));
+        assertThat(recurringDecimal166.fractionToDecimal(1, 7), is("0.(142857)"));
+        assertThat(recurringDecimal166.fractionToDecimal(-1, Integer.MIN_VALUE), is(
+                                                                            "0.0000000004656612873077392578125"));
+
+        assertThat(recurringDecimal166.fractionToDecimal(4, 333), is("0.(012)"));
+        assertThat(recurringDecimal166.fractionToDecimal(4, 333), is("0.(012)"));
+        assertThat(recurringDecimal166.fractionToDecimal(-50, 8), is("-6.25"));
+        assertThat(recurringDecimal166.fractionToDecimal(7, -12), is("-0.58(3)"));
+        assertThat(recurringDecimal166.fractionToDecimal(1, 214748364), is("0.00(000000465661289042462740251655654056577585848337359161441621040707904997124914069194026549138227660723878669455195477065427143370461252966751355553982241280310754777158628319049732085502639731402098131932683780538602845887105337854867197032523144157689601770377165713821223802198558308923834223016478952081795603341592860749337303449725)"));
     }
 }
