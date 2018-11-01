d5963980bce0b8beb03c10dfd33dd998547139f4
haoqiang <haoqiang@oneapm.com>
2018-11-01 14:21:55 +0800

    add solution for Top K Frequent Elements

--- top-100-liked/src/main/java/com/babyachievement/top100/TopKFrequentElements347.java
+++ top-100-liked/src/main/java/com/babyachievement/top100/TopKFrequentElements347.java
@@ -5,7 +5,11 @@
 package com.babyachievement.top100;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * https://leetcode.com/problems/top-k-frequent-elements/description/
@@ -13,6 +17,63 @@ import java.util.List;
 public class TopKFrequentElements347 {
     public List<Integer> topKFrequent(int[] nums, int k) {
         // TODO
-        return new ArrayList<>();
+        final ArrayList<Integer> result = new ArrayList<>();
+        if (nums == null || nums.length == 0 || k == 0) {
+            return result;
+        }
+
+        int max = 0;
+        int min = 0;
+
+
+        return result;
+    }
+
+    List<Integer> topKFrequentBruteForce(int[] nums, int k) {
+        final ArrayList<Integer> result = new ArrayList<>();
+
+        Map<Integer, Integer> vCountMap = new HashMap<>();
+        List<Set<Integer>>    countVSet = new ArrayList<>();
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (vCountMap.containsKey(Integer.valueOf(nums[i]))) {
+                final Integer count = vCountMap.get(nums[i]);
+                vCountMap.put(Integer.valueOf(nums[i]), count.intValue() + 1);
+
+                countVSet.get(count - 1).remove(Integer.valueOf(nums[i]));
+
+                if (countVSet.size() == count) {
+                    Set<Integer> upperSet = new HashSet<>();
+                    upperSet.add(Integer.valueOf(nums[i]));
+                    countVSet.add(upperSet);
+                } else {
+                    countVSet.get(count).add(Integer.valueOf(nums[i]));
+                }
+
+            } else {
+                vCountMap.put(Integer.valueOf(nums[i]), 1);
+                if (countVSet.isEmpty()) {
+                    Set<Integer> upperSet = new HashSet<>();
+                    upperSet.add(Integer.valueOf(nums[i]));
+                    countVSet.add(upperSet);
+                } else {
+                    countVSet.get(0).add(Integer.valueOf(nums[i]));
+                }
+            }
+        }
+
+        for (int i = countVSet.size() - 1; i >= 0 && result.size() < k; i--) {
+            final Set<Integer> set = countVSet.get(i);
+            for (Integer integer : set) {
+                if (result.size() == k) {
+                    break;
+                }
+
+                result.add(integer);
+            }
+        }
+
+        return result;
     }
 }
--- top-100-liked/src/test/java/com/babyachievement/top100/TopKFrequentElements347Test.java
+++ top-100-liked/src/test/java/com/babyachievement/top100/TopKFrequentElements347Test.java
@@ -16,7 +16,7 @@ public class TopKFrequentElements347Test {
     public void topKFrequent() {
         final TopKFrequentElements347 topKFrequentElements347 = new TopKFrequentElements347();
         final List<Integer> integers = topKFrequentElements347
-                .topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2);
+                .topKFrequentBruteForce(new int[]{1, 1, 1, 2, 2, 3}, 2);
         assertThat(integers.size(), is(2));
         assertTrue(integers.contains(1));
         assertTrue(integers.contains(2));
