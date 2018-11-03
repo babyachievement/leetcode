cae57f465fc679cfc5929ddf36bc9560ad2dbd3f
babyachievement <babyachievement@gmail.com>
2018-11-04 00:12:56 +0800

    add solution for Partition Equal Subset Sum

--- top-100-liked/src/main/java/com/babyachievement/top100/PartitionEqualSubsetSum416.java
+++ top-100-liked/src/main/java/com/babyachievement/top100/PartitionEqualSubsetSum416.java
@@ -10,6 +10,51 @@ package com.babyachievement.top100;
 public class PartitionEqualSubsetSum416 {
     public boolean canPartition(int[] nums) {
         // TODO
+
+        int s = 0;
+        for (int num : nums) {
+            s += num;
+        }
+
+        if (s % 2 != 0) {
+            return false;
+        }
+
+        int[] dp = new int[s / 2 + 1];
+
+        return canPartition(nums, 0, s / 2, dp);
+    }
+
+    boolean canPartition(int[] nums, int start, int rest, int[] dp) {
+        if (rest == 0) {
+            return true;
+        }
+
+        if (start >= nums.length) {
+            return false;
+        }
+
+
+        if (rest < 0) {
+            return false;
+        }
+
+        if (dp[rest] == 1) {
+            return true;
+        }
+
+        if (dp[rest] == -1) {
+            return false;
+        }
+
+        for (int i = start; i < nums.length; i++) {
+            if (canPartition(nums, i + 1, rest - nums[i], dp) || canPartition(nums, i + 1, rest, dp)) {
+                dp[rest] = 1;
+                return true;
+            }
+        }
+
+        dp[rest] = -1;
         return false;
     }
 }
