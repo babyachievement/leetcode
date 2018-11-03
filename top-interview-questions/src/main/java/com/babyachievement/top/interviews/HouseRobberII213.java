09446db52b8cfecda1a6944db0999f1ef55f0083
babyachievement <babyachievement@gmail.com>
2018-11-03 11:23:18 +0800

    add solution for House Robber II

--- top-interview-questions/src/main/java/com/babyachievement/top/interviews/HouseRobberII213.java
+++ top-interview-questions/src/main/java/com/babyachievement/top/interviews/HouseRobberII213.java
@@ -1,11 +1,100 @@
 package com.babyachievement.top.interviews;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/house-robber-ii/description/
  */
 public class HouseRobberII213 {
     public int rob(int[] nums) {
-        // TODO
-        return 0;
+        int[][] dp = new int[nums.length][nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            Arrays.fill(dp[i], Integer.MIN_VALUE);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][i] = nums[i];
+        }
+
+        return max(nums, 0, nums.length - 1, dp);
+    }
+
+    public int max(int[] nums, int start, int end, int[][] dp) {
+        if (start > end) {
+            return 0;
+        }
+        if (start == end) {
+            return nums[start];
+        }
+
+        if (dp[start][end] >= 0) {
+            return dp[start][end];
+        }
+
+        int max = 0;
+        int withCurrent = 0;
+        int withoutCurrent = 0;
+        for (int i = start; i <= end; i++) {
+            if (i > 0) {
+                withCurrent = nums[i] + max(nums, i + 2, end, dp);
+            } else {
+                withCurrent = nums[i] + max(nums, i + 2, end - 1, dp);
+            }
+            withoutCurrent = max(nums, i + 1, end, dp);
+            max = Math.max(max, Math.max(withCurrent, withoutCurrent));
+        }
+
+        dp[start][end] = max;
+        return max;
+    }
+
+
+
+
+    public int rob2(int[] nums) {
+        int n = nums.length;
+
+        if(nums.length == 0 || nums == null){
+            return 0;
+        }
+
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        int[] dp = new int[n - 1];
+
+        int res = Integer.MIN_VALUE;
+
+        for(int i = 0 ; i < n - 1; i++){
+            dp[i] = nums[i];
+        }
+
+        res = Math.max(res, calc(dp));
+
+        for(int i = 0 ; i < n - 1; i++){
+            dp[i] = nums[i + 1];
+        }
+        res = Math.max(res, calc(dp));
+        return res;
+    }
+
+    public int calc(int[] A){
+        if(A == null || A.length == 0){
+            return 0;
+        }
+
+        if(A.length == 1)
+            return A[0];
+
+        int[] dp = new int[A.length + 1];
+        dp[0] = 0;
+        dp[1] = A[0];
+        dp[2] = Math.max(A[0], A[1]);
+        for(int i = 3; i <= A.length; i++){
+            dp[i] = Math.max(dp[i - 1], dp[i -2] + A[i - 1]);
+        }
+
+        return dp[A.length];
     }
 }
--- top-interview-questions/src/test/java/com/babyachievement/top/interviews/HouseRobberII213Test.java
+++ top-interview-questions/src/test/java/com/babyachievement/top/interviews/HouseRobberII213Test.java
@@ -10,7 +10,56 @@ public class HouseRobberII213Test {
     @Test
     public void rob() {
         HouseRobberII213 robberII213 = new HouseRobberII213();
-        assertThat(robberII213.rob(new int[]{2,3,2}), is(3));
-        assertThat(robberII213.rob(new int[]{1,2,3,1}), is(4));
+        assertThat(robberII213.rob(new int[]{2, 3, 2}), is(3));
+        assertThat(robberII213.rob(new int[]{1, 2, 3, 1}), is(4));
+        assertThat(robberII213.rob(new int[]{1, 2, 1, 1}), is(3));
+        int[] nums = new int[]{
+                94,
+                40,
+                49,
+                65,
+                21,
+                21,
+                106,
+                80,
+                92,
+                81,
+                679,
+                4,
+                61,
+                6,
+                237,
+                12,
+                72,
+                74,
+                29,
+                95,
+                265,
+                35,
+                47,
+                1,
+                61,
+                397,
+                52,
+                72,
+                37,
+                51,
+                1,
+                81,
+                45,
+                435,
+                7,
+                36,
+                57,
+                86,
+                81,
+                72
+        };
+        System.out.println(robberII213.rob(nums));
+        int[] nums2 = new int[]{
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        assertThat(robberII213.rob(nums2), is(0));
+
     }
 }
