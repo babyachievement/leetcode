ce9ecd311314b89fe3ef1d02a2cf1fe83ca34e91
babyachievement <babyachievement@gmail.com>
2018-11-03 22:41:56 +0800

    add solution for Increasing Triplet Subsequence

--- top-interview-questions/src/main/java/com/babyachievement/top/interviews/IncreasingTripletSubsequence334.java
+++ top-interview-questions/src/main/java/com/babyachievement/top/interviews/IncreasingTripletSubsequence334.java
@@ -9,12 +9,52 @@ package com.babyachievement.top.interviews;
  */
 public class IncreasingTripletSubsequence334 {
     public boolean increasingTriplet(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        int minMiddle = Integer.MAX_VALUE;
 
-        // TODO
+        int i = 0, j = 0;
+
+        for (int l = 1; l < nums.length; l++) {
+            if (j != 0 && nums[l] > nums[j]) {
+                return true;
+            }
+            if (nums[l] < nums[i]) {
+                i = l;
+            }
+
+            if (nums[l] > nums[i]) {
+                if (j == 0) {
+                    j = l;
+                }
+                else {
+                    if (nums[l] < nums[j]) {
+                        j = l;
+                    } else if (nums[l] > nums[j]) {
+                        return true;
+                    }
+                }
+            }
 
 
-        return true;
+        }
+
+
+        return false;
+    }
+
+    public boolean increasingTriplet2(int[] nums) {
+        if(nums == null || nums.length < 3)
+            return false;
+
+        int min = nums[0];
+        int max = Integer.MAX_VALUE;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>max)
+                return true;
+            else if(nums[i]>min && nums[i] < max)
+                max = nums[i];
+            else if(nums[i]<min)
+                min = nums[i];
+        }
+        return false;
     }
 }
