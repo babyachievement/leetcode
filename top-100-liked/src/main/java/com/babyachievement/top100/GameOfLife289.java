71e074535f67a75e6d2c4a0591feba50f1cace1c
babyachievement <babyachievement@gmail.com>
2018-11-04 16:07:35 +0800

    add solution for Game of Live

--- top-100-liked/src/main/java/com/babyachievement/top100/GameOfLife289.java
+++ top-100-liked/src/main/java/com/babyachievement/top100/GameOfLife289.java
@@ -9,6 +9,84 @@ package com.babyachievement.top100;
  */
 public class GameOfLife289 {
     public void gameOfLife(int[][] board) {
-        // TODO
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                int aliveNeighbors = aliveNeighbor(board, i, j);
+
+                if (board[i][j] == 0) {
+                    if (aliveNeighbors == 3) {
+                        board[i][j] = 2;
+                    }
+                } else if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+                    board[i][j] = -1;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != 0) {
+                    if (board[i][j] == -1) {
+                        board[i][j] = 0;
+                    } else {
+                        board[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+    }
+
+    int aliveNeighbor(int[][] board, int i, int j) {
+        int count = 0;
+
+        if (isAlive(board, i - 1, j - 1)) {
+            count++;
+        }
+
+        if (isAlive(board, i - 1, j)) {
+            count++;
+        }
+
+        if (isAlive(board, i - 1, j + 1)) {
+            count++;
+        }
+
+        if (isAlive(board, i, j - 1)) {
+            count++;
+        }
+
+        if (isAlive(board, i, j + 1)) {
+            count++;
+        }
+
+        if (isAlive(board, i + 1, j - 1)) {
+            count++;
+        }
+
+        if (isAlive(board, i + 1, j)) {
+            count++;
+        }
+
+
+        if (isAlive(board, i + 1, j + 1)) {
+            count++;
+        }
+
+        return count;
+    }
+
+    boolean isAlive(int[][] board, int i, int j) {
+        if (i < 0 || i >= board.length) {
+            return false;
+        }
+
+        if (j < 0 || j >= board[0].length) {
+            return false;
+        }
+
+        // 2 表示dead-> live的
+        return board[i][j] != 0 && board[i][j] != 2;
     }
 }
--- top-100-liked/src/main/java/com/babyachievement/top100/SubarraySumEqualsK560.java
+++ top-100-liked/src/main/java/com/babyachievement/top100/SubarraySumEqualsK560.java
@@ -9,7 +9,22 @@ package com.babyachievement.top100;
  */
 public class SubarraySumEqualsK560 {
     public int subarraySum(int[] nums, int k) {
-        // TODO
-        return 0;
+        // TODO improve
+        int[] count = new int[1];
+        for (int i = 0; i < nums.length; i++) {
+            subarraySum(nums, k, i, count);
+        }
+        return count[0];
+    }
+
+    public void subarraySum(int[] nums, int k, int index, int[] count) {
+        if (index >= nums.length) {
+            return;
+        }
+        if (nums[index] == k) {
+            count[0]++;
+        }
+
+        subarraySum(nums, k - nums[index], index + 1, count);
     }
 }
