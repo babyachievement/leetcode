ee8251b3b9b423da2cbfdcc7b169fe139aff4e5c
babyachievement <babyachievement@gmail.com>
2018-11-12 21:01:46 +0800

    add solution for Paint House

--- week02/src/main/java/com/babyachievement/PaintHouse256.java
+++ week02/src/main/java/com/babyachievement/PaintHouse256.java
@@ -1,5 +1,7 @@
 package com.babyachievement;
 
+import java.util.Arrays;
+
 /**
  * https://www.lintcode.com/problem/paint-house/description
  */
@@ -12,13 +14,21 @@ public class PaintHouse256 {
         // write your code here
         int n = costs.length;
 
+        int[] minCost = new int[3];
         for (int i = 0; i < n; i++) {
-            if (i == 0) {
-
+            if (i > 0) {
+                int[] preCost = Arrays.copyOf(minCost, 3);
+                minCost[0] = costs[i][0] + Math.min(preCost[1], preCost[2]);
+                minCost[1] = costs[i][1] + Math.min(preCost[0], preCost[2]);
+                minCost[2] = costs[i][2] + Math.min(preCost[0], preCost[1]);
+            } else {
+                minCost[0] = costs[0][0];
+                minCost[1] = costs[0][1];
+                minCost[2] = costs[0][2];
             }
         }
 
-        // TODO
-        return  0;
+
+        return Math.min(minCost[0], Math.min(minCost[1], minCost[2]));
     }
 }
